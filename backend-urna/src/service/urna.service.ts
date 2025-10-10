1113ebbc6f59262e35889cdfa9e87634ece95f83
Juan Gomes <juangomes@klever.io>
2025-10-10 16:42:24 -0300

    complete develop urna

--- backend-urna/src/service/urna.service.ts
+++ backend-urna/src/service/urna.service.ts
@@ -1,26 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { VotoDto } from '../model/voto.dto';
 import { IdentidadeClientService } from './identidade-client.service';
+import { TokenDto } from '../model/token.dto';
+import { UrnaRepository } from '../repository/urna.repository';
 
 @Injectable()
 export class UrnaService {
-    constructor(private readonly identidadeClient: IdentidadeClientService) { }
+    constructor(private readonly identidadeClient: IdentidadeClientService, private readonly urnaRepository: UrnaRepository) { }
 
-    async registrarVoto(votoDto: VotoDto) {
-        // Exemplo: valida credencial via microserviço de identidade
-        const validacao: any = await this.identidadeClient.validarIdentidade(votoDto.token);
-        if (!validacao || validacao.data?.error) {
-
-            return { error: 'Credencial inválida ou expirada. ' };
+    // RF13: Tela de confirmação
+    async confirmarVoto(votoDto: VotoDto) {
+        const credencial = await this.validarCredencial(votoDto.token);
+        if ('error' in credencial || !credencial.valido) {
+            return { error: 'Credencial inválida ou já utilizada.' };
         }
-        // Lógica de registro anônimo do voto (MVP: apenas retorna sucesso)
-        this.consolidarVotoBase(votoDto);
-        return { message: 'Voto registrado com sucesso.' };
+        // Continua com o processo de confirmação do voto
+        return this.urnaRepository.registrarVoto(votoDto);
     }
-    private consolidarVotoBase(votoDto: VotoDto) {
-        // Lógica para consolidar o voto na base de dados
-        // Esta função é um placeholder e deve ser implementada conforme a lógica de negócio
-        
-        console.log(`Voto para a chapa ${votoDto.slateId} registrado com sucesso.`);
+    async validarCredencial(token: string) {
+        const validacao: TokenDto = await this.identidadeClient.validarIdentidade(token);
+        if (!validacao.valido) {
+            return { error: 'Credencial inválida ou já utilizada.' };
+        }
+        return validacao;
     }
+
+    // RF12: Listar chapas concorrentes
+    async listarChapas(eleicaoId: string) {
+        // Retorna todas as chapas da eleição do banco de dados
+        return await this.urnaRepository.findChapasByElectionId(eleicaoId);
+    }
+
+
+
 }
